54e3324d4998be4bb68a034f7a67939eadbf171a
wjjboy <285701156@qq.com>
2015-11-25 22:57:12 +0800

    init

---
+++ app.js
@@ -0,0 +1,103 @@
+var config = require('./config');
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+var uuid = require('uuid');
+var expressValidator = require('express-validator');
+var ejs = require('ejs');
+var redis = require("redis");
+var RedisStore = require('connect-redis')(session);
+
+//controller
+var filter = require('./routes/filter');
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', ejs.__express);
+app.set('view engine', 'html');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json({ type: 'application/json' })); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(upload.array());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  name: 'session.sid',
+  genid: function (req) {
+    return uuid.v4();
+  },
+  //Forces the session to be saved back to the session store
+  //connect-redis will touch it
+  resave: false,
+  saveUninitialized: false,
+  secret: 'keyboard cat',
+  store: new RedisStore(config.RedisOptions),
+  cookie: {
+    maxAge: config.sessionTime * 1000
+  }
+}));
+
+// catch lose session error handler
+app.use(function (req, res, next) {
+  if (!req.session) {
+    return next(new Error('session null')) // handle error
+  }
+  next() // otherwise continue
+});
+
+//routers
+app.use('/', index);
+app.use('/users', filter.authorize, users);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    error: err
+  });
+});
+
+var server = app.listen(config.port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
+module.exports = app;
---
+++ config.js
@@ -0,0 +1,16 @@
+/**
+ * configuration
+ */
+var config = {
+	port : 3333,
+	sessionTime : 30*60, //s
+	RedisOptions : {
+		host : '127.0.0.1',
+		port : '6379',
+		ttl  : this.sessionTime,
+		prefix : 'pdf-session_'
+	},
+	wkhtmltopdfPath : 'D:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe'
+}
+
+module.exports = config;
